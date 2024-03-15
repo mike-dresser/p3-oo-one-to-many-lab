2a6c76d78894264eb0e1edab9c966247c7b09006
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-15 13:12:51 -0400

    completed

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,56 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner=None) -> None:
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    def __repr__(self) -> str:
+        return f'{self.name} - {self.pet_type} - {self.owner.name}'
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+
+    @pet_type.setter
+    def pet_type(self, type):
+        if type in Pet.PET_TYPES:
+            self._pet_type = type
+        else:
+            raise Exception('Pet must be in list of approved PET_TYPES')
 
 class Owner:
-    pass
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def pets(self):
+        result = []
+        for pet in Pet.all:
+            if pet.owner is self:
+                result.append(pet)
+        return result
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception('Pet must be a instance of <Pet> object!')
+        
+    def get_sorted_pets(self):
+        
+        # result = [pet for pet in self.pets()]
+        # result.sort(key = lambda pet: pet.name)
+        # return result
+ 
+        return sorted([pet for pet in self.pets()], key = lambda pet: pet.name)
+    
+mike = Owner('mike')    
+cat1 = Pet('Tyrone', 'cat', mike)
+cat2 = Pet('Pippy', 'cat', mike)
+
+print(f'pets: {mike.pets()}')
+print(f'sorted: {mike.get_sorted_pets()}')
